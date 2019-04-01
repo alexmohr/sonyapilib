cac1220b7e6ed7532663fbf168d149c717353e80
Edwin Top <edwin@0x20.eu>
2019-04-01 11:20:07 +0200

    add parameter name

--- sonyapilib/device.py
+++ sonyapilib/device.py
@@ -129,8 +129,7 @@ class SonyDevice():
 
     def wakeonlan(self):
         if self.mac is not None:
-            _LOGGER.debug("Waking up %s at %s", self.mac, self.host)
-            wakeonlan.send_magic_packet(self.mac.replace('-',':'), self.host)
+            wakeonlan.send_magic_packet(self.mac, ip_address=self.host)
 
     def update_service_urls(self):
         """ Initalizes the device by reading the necessary resources from it """

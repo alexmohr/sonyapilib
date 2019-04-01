f94e9d82a1e41d5e4044e96c1c87a5d0a3ea696c
Alex <git@mohr.io>
2019-04-01 22:19:53 +0200

    Updated Version number

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ CURRENT_DIR = os.path.dirname(__file__)
 # and be sure to test it firstly using "python setup.py register sdist upload -r pypitest"
 setup(name='sonyapilib',
   packages = ['sonyapilib'], # this must be the same as the name above
-  version = '0.3.10',
+  version = '0.3.11',
   description = 'Lib to control sony devices with their soap api',
   author = 'Alexander Mohr',
   author_email = 'sonyapilib@mohr.io',
   url = 'https://github.com/alexmohr/sonyapilib', # use the URL to the github repo
-  download_url = 'https://codeload.github.com/alexmohr/sonyapilib/tar.gz/0.3.10',
+  download_url = 'https://codeload.github.com/alexmohr/sonyapilib/tar.gz/0.3.11',
   keywords = ['soap', 'sony', 'api'], # arbitrary keywords
   classifiers = [],
   install_requires=[

7f001c2571d2836a675a02f8947eb0bd1a500ddb
Patrick Schönthaler <user1@trumpesports.com>
2019-04-03 20:50:55 +0200

    Do some codestyling in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from __future__ import absolute_import
 import sys
 import os
-from setuptools import setup, find_packages
-# import subprocess
+from setuptools import setup
+
+
 sys.path.insert(0, '.')
 
 CURRENT_DIR = os.path.dirname(__file__)
@@ -12,22 +13,24 @@ CURRENT_DIR = os.path.dirname(__file__)
 # to deploy to pip, please use
 # make pythonpack
 # python setup.py register sdist upload
-# and be sure to test it firstly using "python setup.py register sdist upload -r pypitest"
-setup(name='sonyapilib',
-  packages = ['sonyapilib'], # this must be the same as the name above
-  version = '0.3.11',
-  description = 'Lib to control sony devices with their soap api',
-  author = 'Alexander Mohr',
-  author_email = 'sonyapilib@mohr.io',
-  url = 'https://github.com/alexmohr/sonyapilib', # use the URL to the github repo
-  download_url = 'https://codeload.github.com/alexmohr/sonyapilib/tar.gz/0.3.11',
-  keywords = ['soap', 'sony', 'api'], # arbitrary keywords
-  classifiers = [],
-  install_requires=[
-      'jsonpickle',
-      'setuptools',
-      'requests', 
-      'wakeonlan'
-  ],
-
+# and be sure to test it firstly using
+# "python setup.py register sdist upload -r pypitest"
+setup(
+    name='sonyapilib',
+    packages=['sonyapilib'],  # this must be the same as the name above
+    version='0.3.11',
+    description='Lib to control sony devices with their soap api',
+    author='Alexander Mohr',
+    author_email='sonyapilib@mohr.io',
+    # use the URL to the github repo
+    url='https://github.com/alexmohr/sonyapilib',
+    download_url='https://codeload.github.com/alexmohr/sonyapilib/tar.gz/0.3.11',
+    keywords=['soap', 'sony', 'api'],  # arbitrary keywords
+    classifiers=[],
+    install_requires=[
+        'jsonpickle',
+        'setuptools',
+        'requests',
+        'wakeonlan',
+    ],
 )

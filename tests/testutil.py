4d23c0bdeb8808a20a0ec053bcb0952546c7ca06
Alex <git@mohr.io>
2019-04-06 23:26:00 +0200

    added python-coveralls to travis config.

--- tests/testutil.py
+++ tests/testutil.py
@@ -12,7 +12,7 @@ def read_file(file_name):
 
 def read_file_bin(file_name, size, offset):
     """ Reads a file from disk """
-    with open(os.path.join(__location__, file_name)) as f:
+    with open(os.path.join(__location__, file_name), 'rb') as f:
         f.seek(offset)
         return f.read(size)
 

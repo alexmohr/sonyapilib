4bdbde6dab33760dc68528eb8e6e012bb75e8223
Alex <git@mohr.io>
2019-10-08 10:49:45 +0200

    linting

--- examples/discover_devices.py
+++ examples/discover_devices.py
@@ -1,4 +1,4 @@
-import json
+"""Example to discover services on a device."""
 from sonyapilib.ssdp import SSDPDiscovery
 
 ip = "10.0.0.102"
--- tests/testutil.py
+++ tests/testutil.py
@@ -1,3 +1,4 @@
+"""Helper file to find files in tests"""
 import os.path
 
 __location__ = os.path.realpath(os.path.join(
@@ -5,13 +6,13 @@ __location__ = os.path.realpath(os.path.join(
 
 
 def read_file(file_name):
-    """ Reads a file from disk """
+    """Read a file from disk"""
     with open(os.path.join(__location__, file_name)) as f:
         return f.read()
 
 
 def read_file_bin(file_name, size, offset):
-    """ Reads a file from disk """
+    """Read a file from disk"""
     with open(os.path.join(__location__, file_name), 'rb') as f:
         f.seek(offset)
         return f.read(size)
